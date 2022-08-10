90c37799ac3ab00836c958a17cce1da4cf452622
Abdulkbk <abdoollahikbk@gmail.com>
2022-08-10 18:49:20 +0100

    Authentication setup with passport.js and jwt with routes guarding

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === password) {
+      const { password, ...rest } = user;
+      return rest;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
