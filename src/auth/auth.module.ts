f8f1cbf8cde85e42497f2b382807f9d408411ebc
Abdulkbk <abdoollahikbk@gmail.com>
2022-08-22 18:05:44 +0100

    config jwt secret

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,17 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
+// import { jwtConstants } from './constants';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
+import 'dotenv/config';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: jwtConstants.secret,
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '120s' },
     }),
   ],
